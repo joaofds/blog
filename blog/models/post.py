4e27cf51a4c56bc253102f410721616d52ed847c
Joao Ferreira da Silva <3386349@ifgoiano.local>
2024-02-16 16:02:17 -0300

    posts

--- blog/models/post.py
+++ blog/models/post.py
@@ -1,4 +1,17 @@
 from django.db import models
 
 class PostModel(models.Model):
-    pass
+    titulo = models.CharField(
+        max_length = 120,
+        null = False,
+        blank = False
+    )
+
+    descricao = models.CharField(
+        max_length = 255,
+        null = False,
+        blank = False
+    )
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    objetos = models.Manager()
