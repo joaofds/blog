1342117507747e39a7a845ab54e37c0b4359e633
Joao <jo4o.fds@gmail.com>
2024-02-19 23:26:52 -0300

    mysql settings

--- blog/settings.py
+++ blog/settings.py
@@ -77,6 +77,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'3306',
+    },
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
