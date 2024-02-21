f2f8875e5818ddfb05175955a10f48aa5e1f4944
Joao <jo4o.fds@gmail.com>
2024-02-20 23:12:40 -0300

    change project name to setup

--- setup/urls.py
+++ setup/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for blog project.
+URL configuration for setup project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls.conf import include
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     # importa urls do app website
